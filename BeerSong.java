686c127d5c28701b894bf09834c01e72c5b4234a
JJ <JJ>
2017-01-30 20:00:03 -0500

    Create song method and called in main

--- BeerSong.java
+++ BeerSong.java
@@ -12,13 +12,25 @@ package cse337;
 public class BeerSong {
 
    
-    public void Ninety_Nine_Bottles_of_Beer(){
-        
+    public static void Ninety_Nine_Bottles_of_Beer(){
+        for(int i =99; i>=1; i--)
+        {
+             if(i==1)
+            {
+                System.out.println(i+" bottle of beer on the wall, "+i+" bottle of beer");
+                System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+                break;
+            }
+            
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer");
+            System.out.println("take one down, pass it around, "+(i-1)+" bottles of beer on the wall.");
+            
+        }
     }
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ninety_Nine_Bottles_of_Beer();
     }
     
 }
